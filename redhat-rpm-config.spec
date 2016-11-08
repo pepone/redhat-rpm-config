pepone/redhat-rpm-config

commit d9235d2d90873ff6207cb26d331e979ae9d651fd

Enable hardened_build by default

redhat-rpm-config.spec
@@ -6,7 +6,7 @@
 
 Summary: Red Hat specific rpm configuration files
 Name: redhat-rpm-config
-Version: 27
+Version: 28
 Release: 1%{?dist}
 # No version specified.
 License: GPL+
@@ -135,6 +135,9 @@ install -p -m 755 -t %{buildroot}%{_rpmconfigdir} kmod.prov
 %{_rpmconfigdir}/macros.d/macros.kmp
 
 %changelog
+* Thu Feb 19 2015 Till Maas <opensource@till.name> - 28-1
+- Enable harden flags by default (#1192183)
+
 * Wed Dec 10 2014 Dan Horák <dan[at]danny.cz> - 27-1
 - Explicitly set -mcpu/-mtune for ppc64p7 and ppc64le to override rpm defaults
 

macros
@@ -127,7 +127,9 @@
 # we don't escape symbols '~', '"', etc. so be careful when changing this
 %_hardening_ldflags	-specs=/usr/lib/rpm/redhat/redhat-hardened-ld
 
-#_hardened_build	0
+# Harden packages by default for Fedora 23:
+# https://fedorahosted.org/fesco/ticket/1384 (accepted on 2014-02-11)
+%_hardened_build	1
 %_hardened_cflags	%{?_hardened_build:%{_hardening_cflags}}
 %_hardened_ldflags	%{?_hardened_build:%{_hardening_ldflags}}
 

commit b1a45b244ea644b78b0f626643758a05c0f049b5

-pie is incompatible with static linkage (#1343892, #1287743)
The hardened gcc specs do not handle static linkage, so building
with -static has been broken since commit
d9235d2d90873ff6207cb26d331e979ae9d651fd. Adjust the -ld spec
file to avoid -pie when static linkage is used, as suggested
by Florian Weimer.

redhat-hardened-ld
@@ -1,5 +1,5 @@
 *self_spec:
-+ %{!shared:%{!r:-pie}}
++ %{!static:%{!shared:%{!r:-pie}}}
 
 *link:
 + -z now

redhat-rpm-config.spec
@@ -6,7 +6,7 @@
 
 Summary: Red Hat specific rpm configuration files
 Name: redhat-rpm-config
-Version: 53
+Version: 54
 Release: 1%{?dist}
 # No version specified.
 License: GPL+
@@ -140,6 +140,9 @@ install -p -m 755 -t %{buildroot}%{_rpmconfigdir} kmod.prov
 %{_rpmconfigdir}/macros.d/macros.kmp
 
 %changelog
+* Tue Nov 08 2016 Panu Matilainen <pmatilai@redhat.com> - 54-1
+- -pie is incompatible with static linkage (#1343892, #1287743)
+
 * Mon Nov 07 2016 Panu Matilainen <pmatilai@redhat.com> - 53-1
 - Drop brp-java-repack-jars by request (#1235770)
 - Drop brp-implant-ident-static, unused for 13 years and counting